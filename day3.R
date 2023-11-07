72e9b3b0dde4ba0a3c48f5de956984dcdee67c20
turkimo <binhammadt@gmail.com>
2023-11-07 12:59:33 +0300

    Update day3.R

--- day3.R
+++ day3.R
@@ -6,7 +6,7 @@ library(dplyr)
 library(autoReg)
 
 # Read the dataset 
-data <- read.csv("C:/Users/Shaher/Downloads/dataset.csv", stringsAsFactors = TRUE)
+data <- read.csv("/Users/turkimohammed/Documents/GitHub/causal_survival_sim/dataset.csv", stringsAsFactors = TRUE)
 
 
 # summary of dataset
@@ -39,7 +39,9 @@ ggsurvplot(
 
 KMunicate(fit1, time_scale = seq(0,5,by= 1))
 
-
+# Assign the reference 
+data$Treatment
+data$Treatment <- relevel(data$Treatment, ref = "Control")
 # fit the cox model 
 
 fit2 <- coxph(Surv(eventtime,status)~Treatment+gregion+ miexhep+alpha_feto+ ecog_perf,data=data)
@@ -60,4 +62,23 @@ ggcoxzph(test.ph)
 # Testing influential observations
 
 ggcoxdiagnostics(fit2,type = "dfbeta", linear.predictions = FALSE, ggtheme = theme_bw())
+
+# Create a new data frame for predictions
+# This would typically include different levels of covariates for which you want to plot survival curves
+newdata <- with(data, expand.grid(Treatment = unique(Treatment), gregion = unique(gregion), miexhep = unique(miexhep), alpha_feto = unique(alpha_feto), ecog_perf = unique(ecog_perf)))
+
+# Predict survival using the fitted model
+surv_fit <- survfit(fit2, newdata = newdata)
+
+fit2 <- coxph(Surv(eventtime,status)~Treatment+gregion+ miexhep+alpha_feto+ ecog_perf,data=data)
+
+# Plotting the survival curves
+ggadjustedcurves(
+  fit2,
+  variable = "Treatment",
+  data = data,
+  palette = "hue",
+  ylab = "Survival probability",
+  size = 1,
+  ggtheme = theme_survminer(),)
 
